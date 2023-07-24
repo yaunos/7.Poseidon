71a9c75d86a64aa06fa764e1ae4a1e2de5a63f98
DevPC <a.roche.c@gmail.com>
2023-07-24 20:16:16 +0200

    Initial commit

---
+++ src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -0,0 +1,11 @@
+package com.nnk.springboot.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "rulename")
+public class RuleName {
+    // TODO: Map columns in data table RULENAME with corresponding java fields
+}
